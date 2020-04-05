57f10f91733f57cacf0233fd83781dabcc02454b
KrimsonHAT <fabtroydsounds@gmail.com>
2020-04-05 15:09:18 -0500

    app-php finished about to-do-list

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+    'localhost:3308',
+    'root',
+    '',
+    'php_mysql'
+);
+
+
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php_mysql_crud</title>
+    <!--bootstrap4 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!--font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+    <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+
+</nav>
+    
